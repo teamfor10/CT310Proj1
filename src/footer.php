0bca48c572c7a9d40e97c01c6c0609c770d49ac1
Asa Staven <staven@Asas-Mac-mini.local>
2017-02-12 16:10:54 -0700

    Added header, footer, and style sheet

---
+++ src/footer.php
@@ -0,0 +1,23 @@
+<div id="footer">
+	<div class="row">
+		<div class="col-sm-3 col-xs-12" id="footerLeft"></div>
+		<div class="col-sm-6 col-xs-12" id="footerCenter">
+      <p>This site is part of a CSU <a href="http://www.cs.colostate.edu/~ct310/yr2017sp/">
+        CT 310 Course Project.</a>
+      </p>
+		</div>
+		<!-- End of footer center division -->
+		<!-- End of footer left division -->
+		<div class="col-sm-3 col-xs-12">
+			<!-- <div id="footerRight">
+				<a href="http://www.cs.colostate.edu"> <img src="../res_images/csBuildingLowerRight.png" alt="CS Department" height="48" width="185" style="border: 0px;"></a>
+			</div> -->
+		</div>
+		<!-- End of footer right division -->
+	</div>
+</div>
+
+
+<!-- Close body and html opened in header -->
+</body>
+</html>
---
+++ src/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8 />
+  <meta name="description" content="IFY">
+  <meta name="author" content="Collin Kinchen & Asa Staven">
+  <meta name="keywords" content="HTML,CSS,Web,Development,PHP,ingredients,ct310,fresh">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link href="style.css" rel="stylesheet" type="text/css" />
+  <title>Ingredients For You</title>
+</head>
+
+<!-- Start of page Body -->
+<body>
