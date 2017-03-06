b383ed28b2be5226622259f44821ce26f39f1a05
collinkinchen <collinkinchen@gmail.com>
2017-03-05 23:57:53 -0700

    changes and additions

--- about.php
+++ about.php
@@ -4,7 +4,7 @@
 
 		<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
 				<div id="Asa Staven">
-					<h1 style="text-align:center;">Asa Staven</h1>
+					<h2 style="text-align:center;">Asa Staven</h2>
 					<div  style="text-align:center;" id="logo"><img src="./images/asa.png" height="130px" width="auto"></div>
 					<p style="text-align:center; font-size: 18px;">I'm Asa Staven. I am a Senior at Colorado State University graduating in May.
 						<br />I am studying Applied Computing Technology with a minor in Business Administration.
@@ -16,9 +16,11 @@
 
 		<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
 				<div id="Collin Kinchen">
-				<h1 style="text-align:center;">Collin Kinchen</h1>
-				<div  style="text-align:center;" id="logo"><img src="./images/collinheadshot.jpg" height="100px" width="auto"></div>
-				<p style="text-align:center; font-size: 18px;">Hello my name is Collin Kinchen, I am a Junior at Colorado State University</p>
+				<h2 style="text-align:center;">Collin Kinchen</h2>
+				<div  style="text-align:center;" id="logo"><img src="./images/collinheadshot.jpg" height="130px" width="auto"></div>
+				<p style="text-align:center; font-size: 18px;">Hello my name is Collin Kinchen, I am a Junior at Colorado State University graduating in December
+				<br />I am studying Applied Computing Technology with a minor in Business Administration.
+				<br />In my free time I love to hike and listen to Blink-182</p>
 				</div>
 		</div>
 
--- header.php
+++ header.php
@@ -29,6 +29,11 @@
     <div class="container-fluid" id="myNav">
 
       <div class="navbar-header">
+	  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span> 
+      </button>
         <a class="navbar-brand" href="index.php">Home</a>
       </div>
       <div class="collapse navbar-collapse" id="myNavbar">
